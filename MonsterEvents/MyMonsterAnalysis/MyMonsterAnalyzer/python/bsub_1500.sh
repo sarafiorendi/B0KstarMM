273b4f6f437006cc61c2ef98e381d6411c975f4d
Mauro <mauro.dinardo@cern.ch>
2013-12-18 21:42:14 +0100

    Removed MonsterEvents

--- MonsterEvents/MyMonsterAnalysis/MyMonsterAnalyzer/python/bsub_1500.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-source /afs/cern.ch/user/d/dinardo/MyScripts/Init.sh
-cmsRun /afs/cern.ch/user/d/dinardo/CMSSW_3_3_5/src/MyMonsterAnalysis/MyMonsterAnalyzer/python/MyMonsterAnalysis_1500_cfg.py
