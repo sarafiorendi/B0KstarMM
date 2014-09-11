8cf0c4a1e1e5acfc715013cadf10b03941782e21
Mauro <mauro.dinardo@cern.ch>
2014-09-11 14:39:16 +0200

    Minor

--- B0Analysis/B0KstMuMu/plugins/Scripts/InitAnalysis.sh
+++ B0Analysis/B0KstMuMu/plugins/Scripts/InitAnalysis.sh
@@ -1,11 +1,14 @@
-export ANALYPATH=/Users/dinardo/Documents/MyDocuments/CMS/PhysicsAnalysis/B0Analysis/B0KstMuMu/
+if [ "$#" -ne 1 ] ; then
+    echo @@@ Error: Analysis path is missing @@@
+else
+    export ANALYPATH=$1
 
-export DATAYEAR=2012
-export DATADIR=/nfs/data37/cms/dinardo/Data"$DATAYEAR"B0KstMuMuResults
+    export DATAYEAR=2012
+    export DATADIR=/nfs/data37/cms/dinardo/Data"$DATAYEAR"B0KstMuMuResults
 
-unset DISPLAY
+    unset DISPLAY
 
-echo @@@ Analysis environment variable: $ANALYPATH @@@
-echo @@@ Directory with data: $DATADIR @@@
-echo @@@ Data year: $DATAYEAR @@@
-echo @@@ Unset DISPLAY @@@
+    echo @@@ Analysis environment variable: $ANALYPATH @@@
+    echo @@@ Directory with data: $DATADIR @@@
+    echo @@@ Data year: $DATAYEAR @@@
+    echo @@@ Unset DISPLAY @@@
