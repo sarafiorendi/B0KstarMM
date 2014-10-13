cde29e255e5827484d5792530db8a2fbef5aab1f
Mauro <mauro.dinardo@cern.ch>
2014-10-13 10:52:36 +0200

    Minor

--- B0Analysis/B0KstMuMu/plugins/Scripts/InitAnalysis.sh
+++ B0Analysis/B0KstMuMu/plugins/Scripts/InitAnalysis.sh
@@ -1,15 +1,18 @@
-if [ "$#" -ne 1 ] ; then
-    echo @@@ Error: Analysis path is missing @@@
+if [ "$#" -ne 2 ] ; then
+    echo @@@ Error: Parameter missing @@@
+    echo "Synopsis: InitAnalysis.sh analysis_path unset_display[0=false;1=true]"
 else
     export ANALYPATH=$1
 
     export DATAYEAR=2012
     export DATADIR=/nfs/data37/cms/dinardo/Data"$DATAYEAR"B0KstMuMuResults
 
-    unset DISPLAY
-
     echo @@@ Analysis environment variable: $ANALYPATH @@@
     echo @@@ Directory with data: $DATADIR @@@
     echo @@@ Data year: $DATAYEAR @@@
-    echo @@@ Unset DISPLAY @@@
+
+    if [ "$2" -eq 1 ]; then
+	unset DISPLAY
+	echo @@@ Unset DISPLAY @@@
+    fi
 fi
