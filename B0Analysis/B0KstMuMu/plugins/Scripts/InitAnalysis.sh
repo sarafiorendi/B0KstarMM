35551f82678a4b6d48fc4f493d9c13eb59fe363c
Mauro <mauro.dinardo@cern.ch>
2015-07-15 14:49:18 +0200

    Minor

--- B0Analysis/B0KstMuMu/plugins/Scripts/InitAnalysis.sh
+++ B0Analysis/B0KstMuMu/plugins/Scripts/InitAnalysis.sh
@@ -5,7 +5,7 @@ else
     export ANALYPATH=$1
 
     export DATAYEAR=2012
-    export DATADIR=/nfs/data37/cms/dinardo/Data"$DATAYEAR"B0KstMuMuResults
+    export DATADIR=/nfs/data37/cms/dinardo/Data"$DATAYEAR"B0KstMuMuResults_2012Article
 
     echo @@@ Analysis environment variable: $ANALYPATH @@@
     echo @@@ Directory with data: $DATADIR @@@
