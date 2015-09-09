b0c9cd93ff9b1b88946181f86d9a09f73c04ef6a
Mauro <mauro.dinardo@cern.ch>
2015-09-09 17:48:43 +0200

    New script

---
+++ B0Analysis/B0KstMuMu/plugins/Scripts/runMoveDirs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# First parameter: number of jobs
+# Second parameter: q2 bin index
+
+echo "Parameters: " $1 " and " $2
+
+i=1;
+while [ "$i" -le "$1" ]
+do
+  echo "FLAFB05_"$i"_$2 tmp"
+#  mv FLAFB05_"$i"_$2 tmp
+  i=$((i+1))
+done
