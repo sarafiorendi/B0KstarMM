42fa3d88757390ede02228ae383ffcf6fceaf995
Mauro <mauro.dinardo@cern.ch>
2016-07-26 15:22:15 +0200

    Minor

--- SensorDesign/SolvePoissonPDE3D.m
+++ SensorDesign/SolvePoissonPDE3D.m
@@ -11,19 +11,27 @@
 % rho     = Charge denisty in the bulk [(Coulomb / um^3) / eps0 [F/um]]
 % ItFigIn = Figure iterator input
 
+
+% TODO:
+% 1. Define Sensor and Air volumes
+% 2. Plot potential along a certain z-axis
+
+
 function [potential, ItFigOut] = SolvePoissonPDE3D(Bulk,PitchX,PitchY,...
-    BiasB,BiasS,BiasW,epsR,rho,ItFigIn)
+    BiasB,BiasW,epsR,rho,ItFigIn)
 TStart = cputime; % CPU time at start
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Variable initialization %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
-MetalThick  =   5; % Metalization thickness [um]
-MetalWidthX =  50; % Metalization width along X [um]
+StepMesh    = 5;   % Step to build mesh [um]
+StepSlices  = 5;   % Step to build slices along z [um]
+
+MetalThick  = 5;   % Metalization thickness [um]
+MetalWidthX = 50;  % Metalization width along X [um]
 MetalWidthY = 100; % Metalization width along Y [um]
-SHeight     =   2; % Sensor height [units of bulk thickness]
-Step = 5;
+SHeight     = 2;   % Sensor height [units of bulk thickness]
 
 
 %%%%%%%%%%%%%%%%%%%%
@@ -40,41 +48,41 @@ pdem = createpde(1);
 %%%%%%%%%
 % Row 0 %
 %%%%%%%%%
-[x00,y00,z00] = meshgrid(-MetalWidthX/2:Step:MetalWidthX/2,...
-    -MetalWidthY/2:Step:MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[x00,y00,z00] = meshgrid(-MetalWidthX/2:StepMesh:MetalWidthX/2,...
+    -MetalWidthY/2:StepMesh:MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 x00 = x00(:);
 y00 = y00(:);
 z00 = z00(:);
 
-[xp10,yp10,zp10] = meshgrid(1*PitchX-MetalWidthX/2:Step:1*PitchX+MetalWidthX/2,...
-    -MetalWidthY/2:Step:MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp10,yp10,zp10] = meshgrid(1*PitchX-MetalWidthX/2:StepMesh:1*PitchX+MetalWidthX/2,...
+    -MetalWidthY/2:StepMesh:MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp10 = xp10(:);
 yp10 = yp10(:);
 zp10 = zp10(:);
 
-[xp20,yp20,zp20] = meshgrid(2*PitchX-MetalWidthX/2:Step:2*PitchX+MetalWidthX/2,...
-    -MetalWidthY/2:Step:MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp20,yp20,zp20] = meshgrid(2*PitchX-MetalWidthX/2:StepMesh:2*PitchX+MetalWidthX/2,...
+    -MetalWidthY/2:StepMesh:MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp20 = xp20(:);
 yp20 = yp20(:);
 zp20 = zp20(:);
 
-[xm10,ym10,zm10] = meshgrid(-1*PitchX-MetalWidthX/2:Step:-1*PitchX+MetalWidthX/2,...
-    -MetalWidthY/2:Step:MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm10,ym10,zm10] = meshgrid(-1*PitchX-MetalWidthX/2:StepMesh:-1*PitchX+MetalWidthX/2,...
+    -MetalWidthY/2:StepMesh:MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm10 = xm10(:);
 ym10 = ym10(:);
 zm10 = zm10(:);
 
-[xm20,ym20,zm20] = meshgrid(-2*PitchX-MetalWidthX/2:Step:-2*PitchX+MetalWidthX/2,...
-    -MetalWidthY/2:Step:MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm20,ym20,zm20] = meshgrid(-2*PitchX-MetalWidthX/2:StepMesh:-2*PitchX+MetalWidthX/2,...
+    -MetalWidthY/2:StepMesh:MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm20 = xm20(:);
 ym20 = ym20(:);
@@ -83,41 +91,41 @@ zm20 = zm20(:);
 %%%%%%%%%%
 % Row +1 %
 %%%%%%%%%%
-[x0p1,y0p1,z0p1] = meshgrid(-MetalWidthX/2:Step:MetalWidthX/2,...
-    1*PitchY-MetalWidthY/2:Step:1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[x0p1,y0p1,z0p1] = meshgrid(-MetalWidthX/2:StepMesh:MetalWidthX/2,...
+    1*PitchY-MetalWidthY/2:StepMesh:1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 x0p1 = x0p1(:);
 y0p1 = y0p1(:);
 z0p1 = z0p1(:);
 
-[xp1p1,yp1p1,zp1p1] = meshgrid(1*PitchX-MetalWidthX/2:Step:1*PitchX+MetalWidthX/2,...
-    1*PitchY-MetalWidthY/2:Step:1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp1p1,yp1p1,zp1p1] = meshgrid(1*PitchX-MetalWidthX/2:StepMesh:1*PitchX+MetalWidthX/2,...
+    1*PitchY-MetalWidthY/2:StepMesh:1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp1p1 = xp1p1(:);
 yp1p1 = yp1p1(:);
 zp1p1 = zp1p1(:);
 
-[xp2p1,yp2p1,zp2p1] = meshgrid(2*PitchX-MetalWidthX/2:Step:2*PitchX+MetalWidthX/2,...
-    1*PitchY-MetalWidthY/2:Step:1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp2p1,yp2p1,zp2p1] = meshgrid(2*PitchX-MetalWidthX/2:StepMesh:2*PitchX+MetalWidthX/2,...
+    1*PitchY-MetalWidthY/2:StepMesh:1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp2p1 = xp2p1(:);
 yp2p1 = yp2p1(:);
 zp2p1 = zp2p1(:);
 
-[xm1p1,ym1p1,zm1p1] = meshgrid(-1*PitchX-MetalWidthX/2:Step:-1*PitchX+MetalWidthX/2,...
-    1*PitchY-MetalWidthY/2:Step:1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm1p1,ym1p1,zm1p1] = meshgrid(-1*PitchX-MetalWidthX/2:StepMesh:-1*PitchX+MetalWidthX/2,...
+    1*PitchY-MetalWidthY/2:StepMesh:1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm1p1 = xm1p1(:);
 ym1p1 = ym1p1(:);
 zm1p1 = zm1p1(:);
 
-[xm2p1,ym2p1,zm2p1] = meshgrid(-2*PitchX-MetalWidthX/2:Step:-2*PitchX+MetalWidthX/2,...
-    1*PitchY-MetalWidthY/2:Step:1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm2p1,ym2p1,zm2p1] = meshgrid(-2*PitchX-MetalWidthX/2:StepMesh:-2*PitchX+MetalWidthX/2,...
+    1*PitchY-MetalWidthY/2:StepMesh:1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm2p1 = xm2p1(:);
 ym2p1 = ym2p1(:);
@@ -126,41 +134,41 @@ zm2p1 = zm2p1(:);
 %%%%%%%%%%
 % Row +2 %
 %%%%%%%%%%
-[x0p2,y0p2,z0p2] = meshgrid(-MetalWidthX/2:Step:MetalWidthX/2,...
-    2*PitchY-MetalWidthY/2:Step:2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[x0p2,y0p2,z0p2] = meshgrid(-MetalWidthX/2:StepMesh:MetalWidthX/2,...
+    2*PitchY-MetalWidthY/2:StepMesh:2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 x0p2 = x0p2(:);
 y0p2 = y0p2(:);
 z0p2 = z0p2(:);
 
-[xp1p2,yp1p2,zp1p2] = meshgrid(1*PitchX-MetalWidthX/2:Step:1*PitchX+MetalWidthX/2,...
-    2*PitchY-MetalWidthY/2:Step:2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp1p2,yp1p2,zp1p2] = meshgrid(1*PitchX-MetalWidthX/2:StepMesh:1*PitchX+MetalWidthX/2,...
+    2*PitchY-MetalWidthY/2:StepMesh:2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp1p2 = xp1p2(:);
 yp1p2 = yp1p2(:);
 zp1p2 = zp1p2(:);
 
-[xp2p2,yp2p2,zp2p2] = meshgrid(2*PitchX-MetalWidthX/2:Step:2*PitchX+MetalWidthX/2,...
-    2*PitchY-MetalWidthY/2:Step:2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp2p2,yp2p2,zp2p2] = meshgrid(2*PitchX-MetalWidthX/2:StepMesh:2*PitchX+MetalWidthX/2,...
+    2*PitchY-MetalWidthY/2:StepMesh:2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp2p2 = xp2p2(:);
 yp2p2 = yp2p2(:);
 zp2p2 = zp2p2(:);
 
-[xm1p2,ym1p2,zm1p2] = meshgrid(-1*PitchX-MetalWidthX/2:Step:-1*PitchX+MetalWidthX/2,...
-    2*PitchY-MetalWidthY/2:Step:2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm1p2,ym1p2,zm1p2] = meshgrid(-1*PitchX-MetalWidthX/2:StepMesh:-1*PitchX+MetalWidthX/2,...
+    2*PitchY-MetalWidthY/2:StepMesh:2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm1p2 = xm1p2(:);
 ym1p2 = ym1p2(:);
 zm1p2 = zm1p2(:);
 
-[xm2p2,ym2p2,zm2p2] = meshgrid(-2*PitchX-MetalWidthX/2:Step:-2*PitchX+MetalWidthX/2,...
-    2*PitchY-MetalWidthY/2:Step:2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm2p2,ym2p2,zm2p2] = meshgrid(-2*PitchX-MetalWidthX/2:StepMesh:-2*PitchX+MetalWidthX/2,...
+    2*PitchY-MetalWidthY/2:StepMesh:2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm2p2 = xm2p2(:);
 ym2p2 = ym2p2(:);
@@ -169,41 +177,41 @@ zm2p2 = zm2p2(:);
 %%%%%%%%%%
 % Row -1 %
 %%%%%%%%%%
-[x0m1,y0m1,z0m1] = meshgrid(-MetalWidthX/2:Step:MetalWidthX/2,...
-    -1*PitchY-MetalWidthY/2:Step:-1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[x0m1,y0m1,z0m1] = meshgrid(-MetalWidthX/2:StepMesh:MetalWidthX/2,...
+    -1*PitchY-MetalWidthY/2:StepMesh:-1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 x0m1 = x0m1(:);
 y0m1 = y0m1(:);
 z0m1 = z0m1(:);
 
-[xp1m1,yp1m1,zp1m1] = meshgrid(1*PitchX-MetalWidthX/2:Step:1*PitchX+MetalWidthX/2,...
-    -1*PitchY-MetalWidthY/2:Step:-1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp1m1,yp1m1,zp1m1] = meshgrid(1*PitchX-MetalWidthX/2:StepMesh:1*PitchX+MetalWidthX/2,...
+    -1*PitchY-MetalWidthY/2:StepMesh:-1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp1m1 = xp1m1(:);
 yp1m1 = yp1m1(:);
 zp1m1 = zp1m1(:);
 
-[xp2m1,yp2m1,zp2m1] = meshgrid(2*PitchX-MetalWidthX/2:Step:2*PitchX+MetalWidthX/2,...
-    -1*PitchY-MetalWidthY/2:Step:-1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp2m1,yp2m1,zp2m1] = meshgrid(2*PitchX-MetalWidthX/2:StepMesh:2*PitchX+MetalWidthX/2,...
+    -1*PitchY-MetalWidthY/2:StepMesh:-1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp2m1 = xp2m1(:);
 yp2m1 = yp2m1(:);
 zp2m1 = zp2m1(:);
 
-[xm1m1,ym1m1,zm1m1] = meshgrid(-1*PitchX-MetalWidthX/2:Step:-1*PitchX+MetalWidthX/2,...
-    -1*PitchY-MetalWidthY/2:Step:-1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm1m1,ym1m1,zm1m1] = meshgrid(-1*PitchX-MetalWidthX/2:StepMesh:-1*PitchX+MetalWidthX/2,...
+    -1*PitchY-MetalWidthY/2:StepMesh:-1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm1m1 = xm1m1(:);
 ym1m1 = ym1m1(:);
 zm1m1 = zm1m1(:);
 
-[xm2m1,ym2m1,zm2m1] = meshgrid(-2*PitchX-MetalWidthX/2:Step:-2*PitchX+MetalWidthX/2,...
-    -1*PitchY-MetalWidthY/2:Step:-1*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm2m1,ym2m1,zm2m1] = meshgrid(-2*PitchX-MetalWidthX/2:StepMesh:-2*PitchX+MetalWidthX/2,...
+    -1*PitchY-MetalWidthY/2:StepMesh:-1*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm2m1 = xm2m1(:);
 ym2m1 = ym2m1(:);
@@ -212,41 +220,41 @@ zm2m1 = zm2m1(:);
 %%%%%%%%%%
 % Row -2 %
 %%%%%%%%%%
-[x0m2,y0m2,z0m2] = meshgrid(-MetalWidthX/2:Step:MetalWidthX/2,...
-    -2*PitchY-MetalWidthY/2:Step:-2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[x0m2,y0m2,z0m2] = meshgrid(-MetalWidthX/2:StepMesh:MetalWidthX/2,...
+    -2*PitchY-MetalWidthY/2:StepMesh:-2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 x0m2 = x0m2(:);
 y0m2 = y0m2(:);
 z0m2 = z0m2(:);
 
-[xp1m2,yp1m2,zp1m2] = meshgrid(1*PitchX-MetalWidthX/2:Step:1*PitchX+MetalWidthX/2,...
-    -2*PitchY-MetalWidthY/2:Step:-2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp1m2,yp1m2,zp1m2] = meshgrid(1*PitchX-MetalWidthX/2:StepMesh:1*PitchX+MetalWidthX/2,...
+    -2*PitchY-MetalWidthY/2:StepMesh:-2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp1m2 = xp1m2(:);
 yp1m2 = yp1m2(:);
 zp1m2 = zp1m2(:);
 
-[xp2m2,yp2m2,zp2m2] = meshgrid(2*PitchX-MetalWidthX/2:Step:2*PitchX+MetalWidthX/2,...
-    -2*PitchY-MetalWidthY/2:Step:-2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xp2m2,yp2m2,zp2m2] = meshgrid(2*PitchX-MetalWidthX/2:StepMesh:2*PitchX+MetalWidthX/2,...
+    -2*PitchY-MetalWidthY/2:StepMesh:-2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xp2m2 = xp2m2(:);
 yp2m2 = yp2m2(:);
 zp2m2 = zp2m2(:);
 
-[xm1m2,ym1m2,zm1m2] = meshgrid(-1*PitchX-MetalWidthX/2:Step:-1*PitchX+MetalWidthX/2,...
-    -2*PitchY-MetalWidthY/2:Step:-2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm1m2,ym1m2,zm1m2] = meshgrid(-1*PitchX-MetalWidthX/2:StepMesh:-1*PitchX+MetalWidthX/2,...
+    -2*PitchY-MetalWidthY/2:StepMesh:-2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm1m2 = xm1m2(:);
 ym1m2 = ym1m2(:);
 zm1m2 = zm1m2(:);
 
-[xm2m2,ym2m2,zm2m2] = meshgrid(-2*PitchX-MetalWidthX/2:Step:-2*PitchX+MetalWidthX/2,...
-    -2*PitchY-MetalWidthY/2:Step:-2*PitchY+MetalWidthY/2,...
-    Bulk:Step:Bulk+MetalThick);
+[xm2m2,ym2m2,zm2m2] = meshgrid(-2*PitchX-MetalWidthX/2:StepMesh:-2*PitchX+MetalWidthX/2,...
+    -2*PitchY-MetalWidthY/2:StepMesh:-2*PitchY+MetalWidthY/2,...
+    Bulk:StepMesh:Bulk+MetalThick);
 
 xm2m2 = xm2m2(:);
 ym2m2 = ym2m2(:);
@@ -255,13 +263,35 @@ zm2m2 = zm2m2(:);
 %%%%%%%%%%%%%%%%
 % Whole volume %
 %%%%%%%%%%%%%%%%
-[x,y,z] = meshgrid(-2*PitchX-MetalWidthX:Step:2*PitchX+MetalWidthX,...
-    -2*PitchY-MetalWidthY:Step:2*PitchY+MetalWidthY,...
-    0:Step:Bulk*SHeight);
+[x,y,z] = meshgrid(-2*PitchX-MetalWidthX:StepMesh:2*PitchX+MetalWidthX,...
+    -2*PitchY-MetalWidthY:StepMesh:2*PitchY+MetalWidthY,...
+    0:StepMesh:Bulk*SHeight);
 
 x = x(:);
 y = y(:);
 z = z(:);
+
+%%%%%%%%%%%%%%
+% Air volume %
+%%%%%%%%%%%%%%
+[xAir,yAir,zAir] = meshgrid(-2*PitchX-MetalWidthX:StepMesh:2*PitchX+MetalWidthX,...
+    -2*PitchY-MetalWidthY:StepMesh:2*PitchY+MetalWidthY,...
+    Bulk:StepMesh:Bulk*SHeight);
+
+xAir = xAir(:);
+yAir = yAir(:);
+zAir = zAir(:);
+
+%%%%%%%%%%%%%%%%%
+% Sensor volume %
+%%%%%%%%%%%%%%%%%
+[xSen,ySen,zSen] = meshgrid(-2*PitchX-MetalWidthX:StepMesh:2*PitchX+MetalWidthX,...
+    -2*PitchY-MetalWidthY:StepMesh:2*PitchY+MetalWidthY,...
+    0:StepMesh:Bulk);
+
+xSen = xSen(:);
+ySen = ySen(:);
+zSen = zSen(:);
 
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -283,19 +313,27 @@ zPx = [zm2m2' zm1m2' z0m2' zp1m2' zp2m2'...
        zm2p1' zm1p1' z0p1' zp1p1' zp2p1'...
        zm2p2' zm1p2' z0p2' zp1p2' zp2p2']';
 
-shp = alphaShape(xPx,yPx,zPx);
+shpPx = alphaShape(xPx,yPx,zPx);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%
 % Create final volume %
 %%%%%%%%%%%%%%%%%%%%%%%
-in = inShape(shp,x,y,z);
+in = inShape(shpPx,x,y,z);
 x = x(~in);
 y = y(~in);
 z = z(~in);
-shp = alphaShape(x,y,z);
+%in = inShape(shpPx,xAir,yAir,zAir);
+%xAir = xAir(~in);
+%yAir = yAir(~in);
+%zAir = zAir(~in);
 
-[elements,nodes] = boundaryFacets(shp);
+%x = [xAir' xSen']';
+%y = [yAir' ySen']';
+%z = [zAir' zSen']';
+
+shpAll = alphaShape(x,y,z);
+[elements,nodes] = boundaryFacets(shpAll);
 nodes = nodes';
 elements = elements';
 
@@ -306,28 +344,25 @@ elements = elements';
 geometryFromMesh(pdem,nodes,elements);
 
 
-%%%%%%%%%
-% Plots %
-%%%%%%%%%
+figure(ItFigIn);
 h = pdegplot(pdem,'FaceLabels','on');
 h(1).FaceAlpha = 0.5;
+title('Geometry');
+xlabel('X [\mum]');
+ylabel('Y [\mum]');
+zlabel('Z [\mum]');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%
 % Boundary conditions %
 %%%%%%%%%%%%%%%%%%%%%%%
-% Bottom edge
+% Initialisation for all faces
+for i=1:(5*5*6+6)
+    applyBoundaryCondition(pdem,'face',i,'h',1,'r',0);
+end
+
+% Bottom face
 applyBoundaryCondition(pdem,'face',1,'h',1,'r',BiasB);
-% Top edge
-applyBoundaryCondition(pdem,'face',2,'h',1,'r',0);
-% Left edge
-applyBoundaryCondition(pdem,'face',4,'h',1,'r',0);
-% Front edge
-applyBoundaryCondition(pdem,'face',9,'h',1,'r',0);
-% Back edge
-applyBoundaryCondition(pdem,'face',15,'h',1,'r',0);
-% Right edge
-applyBoundaryCondition(pdem,'face',17,'h',1,'r',0);
 
 % Central pixel
 applyBoundaryCondition(pdem,'face',14,'h',1,'r',BiasW);
@@ -347,7 +382,7 @@ msh = generateMesh(pdem,'GeometricOrder','quadratic');
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 % Solve Poisson equation %
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-specifyCoefficients(pdem,'m',0,'d',0,'c',1,'a',0,'f',0);
+specifyCoefficients(pdem,'m',0,'d',0,'c',epsR,'a',0,'f',rho);
 potential = solvepde(pdem);
 
 
@@ -366,6 +401,7 @@ ItFigIn = ItFigIn + 1;
 figure(ItFigIn);
 u = potential.NodalSolution;
 pdeplot3D(pdem,'colormapdata',u);
+axis on;
 title('Delaunay mesh');
 xlabel('X [\mum]');
 ylabel('Y [\mum]');
@@ -374,12 +410,12 @@ zlabel('Z [\mum]');
 ItFigIn = ItFigIn + 1;
 figure(ItFigIn);
 colormap jet;
-[x,y,z] = meshgrid(-PitchX:Step:PitchX,...
-    -PitchY:Step:PitchY,...
-    0:Step:Bulk*3/2);
+[x,y,z] = meshgrid(-2*PitchX-MetalWidthX/2:StepMesh:2*PitchX+MetalWidthX/2,...
+    -2*PitchY-MetalWidthY/2:StepMesh:2*PitchY+MetalWidthY/2,...
+    0:StepSlices:Bulk*3/2);
 V = interpolateSolution(potential,x,y,z);
 V = reshape(V,size(x));
-contourslice(x,y,z,V,[],[],0:Step:Bulk);
+contourslice(x,y,z,V,[],[],0:StepMesh:Bulk);
 title('Potential and its gradient');
 xlabel('X [\mum]');
 ylabel('Y [\mum]');
